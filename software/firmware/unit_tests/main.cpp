695205a7d695bcfc2cb5a42c7fee75746ea834b8
erichstuder <richu_@hotmail.com>
2023-12-27 13:51:59 +0100

    some cleanup

--- software/firmware/unit_tests/main.cpp
+++ software/firmware/unit_tests/main.cpp
@@ -13,25 +13,15 @@ TEST(DummyTest, ShouldPass) {
 #include <Arduino.h>
 
 void setup() {
-	// should be the same value as for the `test_speed` option in "platformio.ini"
-	// default value is test_speed=115200
-	//delay(1000);
-	
 	// Note: At startup some rubbish is written to the serial output.
 	// That is output from the bootloader at baudrate 74880.
-	// It can be ignored, but to read it set the speed here and test_speed in platformio.ini to 74880.
+	// It can be ignored, but to read it set the speed here and test_speed in platformio.ini to bootloader baudrate.
 	// see: https://docs.espressif.com/projects/esptool/en/latest/esp8266/esptool/serial-connection.html
 	Serial.begin(38400); //see: platformio.ini test_speed
 	Serial.print("\nAbove you can see output from bootloader => ignore.\n");
 	Serial.print("It is probably shown as rubbish due to wrong baudrate (bootloader has 74880).\n\n");
 
-	//delay(1000);
-
-	//Serial.println("start");
-
 	::testing::InitGoogleTest();
-	// if you plan to use GMock, replace the line above with
-	// ::testing::InitGoogleMock();
 }
 
 void loop() {
